0d2f324a2175668c3fc8df0f944f2b97c1adf749
Matt Bryson <matt.bryson@skinkers.com>
2012-09-21 08:35:40 +0100

    Added a UL renderer that works with the bootstrap pagination CSS

---
+++ src/jquery.pagination.renderer.ul.js
@@ -0,0 +1,93 @@
+/**
+	 * @class UL Renderer to work with the Bootstrap UI components.
+	 */
+	$.PaginationRenderers.ulRenderer = function(maxentries, opts) {
+		this.maxentries = maxentries;
+		this.opts = opts;
+		this.pc = new $.PaginationCalculator(maxentries, opts);
+	}
+	$.extend($.PaginationRenderers.ulRenderer.prototype, {
+		/**
+		 * Helper function for generating a LI tag
+		 * @param {Number} page_id The page id for the new item
+		 * @param {Number} current_page 
+		 * @param {Object} appendopts Options for the new item: text and classes
+		 * @returns {jQuery} jQuery object containing the link
+		 */
+		createLink:function(page_id, current_page, appendopts){
+			var li, lnk, np = this.pc.numPages();
+			page_id = page_id<0?0:(page_id<np?page_id:np-1); // Normalize page id to sane value
+			appendopts = $.extend({text:page_id+1, classes:""}, appendopts||{});
+			
+			
+			if(page_id == current_page){
+				li = $("<li class='active'><span>" + appendopts.text + "</span></li>");
+			}
+			else
+			{
+				li = $("<li></li>");
+				lnk = $("<a>" + appendopts.text + "</a>")
+					.attr('href', this.opts.link_to.replace(/__id__/,page_id))
+					.data('page_id', page_id);
+				
+				if(appendopts.classes)
+				{ 
+					lnk.addClass(appendopts.classes); 
+				}
+					
+				li.append(lnk);
+			}
+			
+			return li;
+		},
+		// Generate a range of numeric links 
+		appendRange:function(container, current_page, start, end, opts) {
+			var i;
+			for(i=start; i<end; i++) {
+				this.createLink(i, current_page, opts).appendTo(container);
+			}
+		},
+		getLinks:function(current_page, eventHandler) {
+			var begin, end,
+				interval = this.pc.getInterval(current_page),
+				np = this.pc.numPages(),
+				control = $("<div class='pagination'><ul></ul></div>"),
+				fragment = control.find("ul");
+				
+			// Generate "Previous"-Link
+			if(this.opts.prev_text && (current_page > 0 || this.opts.prev_show_always)){
+				fragment.append(this.createLink(current_page-1, current_page, {text:this.opts.prev_text, classes:"prev"}));
+			}
+			// Generate starting points
+			if (interval.start > 0 && this.opts.num_edge_entries > 0)
+			{
+				end = Math.min(this.opts.num_edge_entries, interval.start);
+				this.appendRange(fragment, current_page, 0, end, {classes:'sp'});
+				if(this.opts.num_edge_entries < interval.start && this.opts.ellipse_text)
+				{
+					$("<li><span>"+this.opts.ellipse_text+"</span></li>").appendTo(fragment);
+				}
+			}
+			// Generate interval links
+			this.appendRange(fragment, current_page, interval.start, interval.end);
+			// Generate ending points
+			if (interval.end < np && this.opts.num_edge_entries > 0)
+			{
+				if(np-this.opts.num_edge_entries > interval.end && this.opts.ellipse_text)
+				{
+					$("<li><span>"+this.opts.ellipse_text+"</span></li>").appendTo(fragment);
+				}
+				begin = Math.max(np-this.opts.num_edge_entries, interval.end);
+				this.appendRange(fragment, current_page, begin, np, {classes:'ep'});
+				
+			}
+			// Generate "Next"-Link
+			if(this.opts.next_text && (current_page < np-1 || this.opts.next_show_always)){
+				fragment.append(this.createLink(current_page+1, current_page, {text:this.opts.next_text, classes:"next"}));
+			}
+			$('a', fragment).click(eventHandler);
+			
+			return control;
+		}
+	});
+	
